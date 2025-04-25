a525224194ca2d0e8bceedc67c138d804d6676dc
maamokun <maamo-kun@outlook.com>
2025-04-25 12:26:26 +0900

    Improve factchecking capabilities maybe/

--- src/handlers/mention.ts
+++ src/handlers/mention.ts
@@ -67,7 +67,7 @@ export async function handleMention(message: Message) {
 
     const { text } = await generateText({
         model: xai("grok-3-mini"),
-        prompt: `You have been asked a question within a Discord server. With this context in mind, answer the question as if you were a human. Answer using the language the prompt was written in. Do not show your own character, just reply to the prompt. Users may also be asking you a general question unrelated to the chat, in that case you may ignore the context provided. However, whenever possible take the chat context into consideration. Here is the context: ${JSON.stringify(history)}. Here is the question: ${prompt}`,
+        prompt: `You have been asked a question within a Discord server. With this context in mind, answer the question as if you were a human. Answer using the language the prompt was written in. Do not show your own character, just reply to the prompt. Users may also be asking you a general question unrelated to the chat, in that case you may ignore the context provided. However, whenever possible take the chat context into consideration. Users may also ask questions such as "factcheck" ans "is this true" and if that hapens, it is most likely that you have been tasked to evaluate a stetement made by a user in the chat. Find the statement, and see if it is true or not, giving reasons why. Here is the context: ${JSON.stringify(history)}. Here is the question: ${prompt}`,
     });
 
     await reply.edit({ content: text });
