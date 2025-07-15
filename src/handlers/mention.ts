734d936ff997a59a97b7d379754293fbda36c000
maamokun/MikanDev <sachadeco214@gmail.com>
2025-07-15 21:12:00 +0900

    fix: bad prompting method

--- src/handlers/mention.ts
+++ src/handlers/mention.ts
@@ -62,12 +62,17 @@ export async function handleMention(message: Message) {
     const { text } = await generateText({
         model: xai("grok-3-mini"),
         system: "You have been asked a question within a Discord server. With this context in mind, answer the question as if you were a human. Answer using the language the prompt was written in. Do not show your own character, just reply to the prompt. Users may also be asking you a general question unrelated to the chat, in that case you may ignore the context provided. However, whenever possible take the chat context into consideration. Users may also ask questions such as \"factcheck\" ans \"is this true\" and if that hapens, it is most likely that you have been tasked to evaluate a stetement made by a user in the chat. Find the statement, and see if it is true or not, giving reasons why.",
-        prompt: prompt,
-        messages: history.map((message) => ({
-            role: message.author.id === process.env.BOT_ID ? "assistant" : "user",
-            content: message.content,
-            name: message.author,
-        })),
+        messages: [
+            ...history.map((message) => ({
+                role: message.author.id === process.env.BOT_ID ? "assistant" : "user",
+                content: message.content,
+                name: message.author,
+            })),
+            {
+                role: "user",
+                content: prompt,
+            },
+        ],
     });
 
     await reply.edit({ content: text });
