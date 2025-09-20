4c8b5a93c06ff2f093f749f1747d38be0e49925c
maamokun <sachadeco214@gmail.com>
2025-09-20 13:28:02 +0900

    feat: Grok 4 via Vercel

--- src/handlers/mention.ts
+++ src/handlers/mention.ts
@@ -1,6 +1,6 @@
 import type { Message } from "discord.js";
 import { xai } from "@ai-sdk/xai";
-import { generateText } from "ai";
+import { generateText, ModelMessage } from "ai";
 
 async function createMessageHistory(
     message: Message,
@@ -60,7 +60,7 @@ export async function handleMention(message: Message) {
     const history = await createMessageHistory(message);
 
     const { text } = await generateText({
-        model: xai("grok-3-mini"),
+        model: "xai/grok-4-fast-non-reasoning",
         system: "You have been asked a question within a Discord server. With this context in mind, answer the question as if you were a human. Answer using the language the prompt was written in. Do not show your own character, just reply to the prompt. Users may also be asking you a general question unrelated to the chat, in that case you may ignore the context provided. However, whenever possible take the chat context into consideration. Users may also ask questions such as \"factcheck\" ans \"is this true\" and if that hapens, it is most likely that you have been tasked to evaluate a stetement made by a user in the chat. Find the statement, and see if it is true or not, giving reasons why.",
         messages: [
             ...history.map((message) => ({
@@ -72,7 +72,7 @@ export async function handleMention(message: Message) {
                 role: "user",
                 content: prompt,
             },
-        ],
+        ] as ModelMessage[],
     });
 
     await reply.edit({ content: text });
