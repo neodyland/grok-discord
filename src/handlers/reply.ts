50bb78a16cf80b4d543f0049ecb60ab158c44755
maamokun <sachadeco214@gmail.com>
2025-09-20 13:37:32 +0900

    fix: use new model in replies

--- src/handlers/reply.ts
+++ src/handlers/reply.ts
@@ -36,7 +36,7 @@ export async function handleReply(message: Message) {
     const history = await createMessageHistory(message);
 
     const { text } = await generateText({
-        model: xai("grok-3-mini"),
+        model: "xai/grok-4-fast-non-reasoning",
         system: "You have been asked a question within a Discord server. With this context in mind, answer the question as if you were a human. Answer using the language the prompt was written in. Do not show your own character, just reply to the prompt. Users may also be asking you a general question unrelated to the chat, in that case you may ignore the context provided. However, whenever possible take the chat context into consideration. Users may also ask questions such as \"factcheck\" ans \"is this true\" and if that hapens, it is most likely that you have been tasked to evaluate a stetement made by a user in the chat. Find the statement, and see if it is true or not, giving reasons why.",
         messages: [
             ...history.map((message) => ({
